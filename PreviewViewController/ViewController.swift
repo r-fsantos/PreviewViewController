b8f84e08a328de311ea3c42494572b1ff5fc29b4
renato <felicio95.jr@gmail.com>
2022-10-08 14:53:39 -0300

    Initial Commit

---
+++ PreviewViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreviewViewController
+//
+//  Created by Renato F. dos Santos Jr on 08/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
